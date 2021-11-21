709076f5b3f7eac066d879dc25d30b264811f5b9
Morteza Nedaei <nedaei1375@gmail.com>
2021-11-21 17:58:24 +0330

    add onPress to interface

--- src/components/AirtourLogo/AirtourLogo.tsx
+++ src/components/AirtourLogo/AirtourLogo.tsx
@@ -2,24 +2,13 @@ import React from 'react';
 import { images } from '../../assets';
 import FastImage from 'react-native-fast-image';
 import { Image } from '../Image';
-import { TouchableWithoutFeedback, View } from 'react-native';
-// import { NavHandler } from '../../navigation/navConfig';
-// import { ScreenNames } from '../../resources/strings';
-// import { useAppDispatch } from '../../redux/store';
-// import { changeCurrentApp } from '../../redux/slices/globalSlice';
+import {GestureResponderEvent, TouchableWithoutFeedback, View} from 'react-native';
 import { isAppStore } from '../../utils/Platform';
 
-export const AirtourLogo = React.memo((props: any) => {
-  // const dispatch = useAppDispatch();
-  // const onPress = useCallback(() => {
-  //   dispatch(changeCurrentApp(null));
-  //   NavHandler.replace({
-  //     name: ScreenNames.MainScreenDrawer,
-  //     params: {},
-  //   });
-  // }, []);
+export const AirtourLogo = React.memo((props: { onPress: ((event: GestureResponderEvent) => void) | undefined }) => {
+    const {onPress} = props;
   return (
-    <TouchableWithoutFeedback onPress={props?.onPress}>
+    <TouchableWithoutFeedback onPress={onPress}>
       <View>
         {isAppStore ? null : (
           <Image

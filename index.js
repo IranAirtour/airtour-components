b170d0b22dad967dd39b708048bc6246a50cba09
Morteza Nedaei <nedaei1375@gmail.com>
2021-11-21 12:21:19 +0330

    add resources to base components

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-export * from './src/components/index';
+export * from './src/components';
 
 AppRegistry.registerComponent(appName, () => App);

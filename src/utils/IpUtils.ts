8947ee9043132101c6597ecc5f0ea999511e9228
Morteza Nedaei <nedaei1375@gmail.com>
2021-12-04 12:19:24 +0330

    get public ip and country by api call + add some document comment

--- src/utils/IpUtils.ts
+++ src/utils/IpUtils.ts
@@ -1,8 +1,17 @@
-import publicIP from 'react-native-public-ip';
+/**
+ * required API_BASE_URL(s) and ACCESS_KEY to get ip and it's related country
+ */
+const IP_API_BASE_URL = 'https://api.ipify.org';
+const IP_COUNTRY_BASE_URL = 'http://api.ipapi.com';
+const IP_COUNTRY_API_ACCESS_KEY = '64c6bb9dc59f45ccd49ac1290ad01977';
 
+/**
+ * get country object based on received ip
+ * @param ip
+ */
 export const getCountryByIp = (ip: string): Promise<string> => {
     return fetch(
-        `http://api.ipapi.com/${ip}?access_key=64c6bb9dc59f45ccd49ac1290ad01977&format=1`,
+        `${IP_COUNTRY_BASE_URL + '/' + ip + '?access_key=' + IP_COUNTRY_API_ACCESS_KEY + '&format=1'}`,
     )
         .then(response => response.json())
         .then((responseJson: { country_name: string }) => {
@@ -15,13 +24,19 @@ export const getCountryByIp = (ip: string): Promise<string> => {
         });
 };
 
-export const getPublicIp = (): Promise<string> => {
-    return publicIP()
+/**
+ * get public ip
+ * @param api: using custom api to get public ip
+ */
+export const getPublicIp = (api?: string): Promise<string> => {
+    return fetch(api ?? IP_API_BASE_URL)
+        .then(response => response.text())
         .then((ip: string) => {
+            console.log('public_ip: ', ip);
             return Promise.resolve(ip);
         })
         .catch(error => {
-            console.log(error);
+            console.error(error);
             return Promise.reject(error);
         });
 };

231eb2ac29068906d6d00c45a7127fb6b0904be2
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-18 16:46:40 +0330

    handle logo by props from s.rahimi.airtour

--- src/components/AirtourLogo/AirtourLogo.tsx
+++ src/components/AirtourLogo/AirtourLogo.tsx
@@ -3,30 +3,28 @@ import { images } from '../../assets';
 import FastImage from 'react-native-fast-image';
 import { Image } from '../Image';
 import {GestureResponderEvent, TouchableWithoutFeedback, View} from 'react-native';
-import { isAppStore } from '../../utils/Platform';
-
-export const AirtourLogo = React.memo((props: { onPress: ((event: GestureResponderEvent) => void) | undefined }) => {
-    const {onPress} = props;
-  return (
-    <TouchableWithoutFeedback onPress={onPress}>
-      <View>
-        {isAppStore ? null : (
-          <Image
-            source={images.logo}
-            defaultSource={images.logo}
-            style={{
-              height: 30,
-              width: 100,
-              // aspectRatio: 3.3,
-              priority: FastImage.priority.high,
-              marginEnd: 20,
-              // backgroundColor: 'red',
-            }}
-            ImageComponent={FastImage}
-            resizeMode={FastImage.resizeMode.contain}
-          />
-        )}
-      </View>
-    </TouchableWithoutFeedback>
-  );
+export const AirtourLogo = React.memo((props: { onPress: ((event: GestureResponderEvent) => void) | undefined ; showLogo:boolean }) => {
+    const {onPress,showLogo} = props;
+    return (
+        <TouchableWithoutFeedback onPress={onPress}>
+            <View>
+                {!showLogo ? null : (
+                    <Image
+                        source={images.logo}
+                        defaultSource={images.logo}
+                        style={{
+                            height: 30,
+                            width: 100,
+                            // aspectRatio: 3.3,
+                            priority: FastImage.priority.high,
+                            marginEnd: 20,
+                            // backgroundColor: 'red',
+                        }}
+                        ImageComponent={FastImage}
+                        resizeMode={FastImage.resizeMode.contain}
+                    />
+                )}
+            </View>
+        </TouchableWithoutFeedback>
+    );
 });
